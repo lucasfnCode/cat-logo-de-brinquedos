6b2a4ef6bfe762fc30b4ca858656599e23e0c9d7
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2025-05-04 19:09:26 -0300

    CHORE: criação do projeto loja de brinquedos disciplina de desenvolvimento web 3

---
+++ src/main/java/com/br/semesperanca/loja_de_brinquedos/LojaDeBrinquedosApplication.java
@@ -0,0 +1,13 @@
+package com.br.semesperanca.loja_de_brinquedos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeBrinquedosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeBrinquedosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/semesperanca/loja_de_brinquedos/LojaDeBrinquedosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.semesperanca.loja_de_brinquedos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeBrinquedosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
