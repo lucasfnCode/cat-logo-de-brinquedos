6a195ac2d7b350648219d39c8327d386448df5a9
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2025-05-30 10:59:17 -0300

    :recycle: REFACTOR: adequação do script para criação de entidade teammembers

--- script.sql
+++ script.sql
@@ -2,19 +2,27 @@ CREATE DATABASE loja_de_brinquedos;
 
 USE loja_de_brinquedos;
 
-CREATE TABLE category (
-                          id          INT PRIMARY KEY AUTO_INCREMENT NOT NULL ,
-                          name        VARCHAR(255) NOT NULL
+CREATE TABLE categories (
+                  id          INT PRIMARY KEY AUTO_INCREMENT NOT NULL ,
+                  name        VARCHAR(255) NOT NULL,
+                  image       MEDIUMTEXT
 );
 
 CREATE TABLE toys (
-                      id          INT PRIMARY KEY AUTO_INCREMENT NOT NULL ,
-                      code        VARCHAR(50) NOT NULL,
-                      description VARCHAR(100) NOT NULL,
-                      brand       VARCHAR(100) NOT NULL,
-                      image       VARCHAR(255),
-                      price       DECIMAL(10,2) NOT NULL,
-                      details     TEXT,
-                      category_id INT NOT NULL,
-                      FOREIGN KEY (category_id) REFERENCES category(id)
+                  id          INT PRIMARY KEY AUTO_INCREMENT NOT NULL ,
+                  code        VARCHAR(50) NOT NULL,
+                  description VARCHAR(100) NOT NULL,
+                  brand       VARCHAR(100) NOT NULL,
+                  image       MEDIUMTEXT,
+                  price       DECIMAL(10,2) NOT NULL,
+                  details     TEXT,
+                  category_id INT NOT NULL,
+                  FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE team_members (
+                  id          INT PRIMARY KEY AUTO_INCREMENT,
+                  name        VARCHAR(255) NOT NULL,
+                  ra          VARCHAR(50) NOT NULL,
+                  image       MEDIUMTEXT
 );
